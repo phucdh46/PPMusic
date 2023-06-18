5d5ac6814d16ad64962f1fd5933589d10fa7075a
PhucDH <phucdh@iotlink.com.vn>
2023-06-18 14:09:16 +0700

    [Feature] Init project android

---
+++ app/src/main/java/com/example/musicplayerv2/ui/sign_in/data/LoginDataSource.kt
@@ -0,0 +1,24 @@
+package com.example.musicplayerv2.ui.sign_in.data
+
+import com.example.musicplayerv2.ui.sign_in.data.model.LoggedInUser
+import java.io.IOException
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+class LoginDataSource {
+
+    fun login(username: String, password: String): Result<LoggedInUser> {
+        try {
+            // TODO: handle loggedInUser authentication
+            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
+            return Result.Success(fakeUser)
+        } catch (e: Throwable) {
+            return Result.Error(IOException("Error logging in", e))
+        }
+    }
+
+    fun logout() {
+        // TODO: revoke authentication
+    }
+}
---
+++ app/src/main/java/com/example/musicplayerv2/ui/sign_in/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.musicplayerv2.ui.sign_in.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
