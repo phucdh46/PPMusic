8a3d1435506dfa9f3cd5323b9e1a7b6e2614ab89
dhp <june041996@gmail.com>
2024-05-19 18:08:51 +0700

    save playlist view type to datastore preference

--- app/src/main/java/com/dhp/musicplayer/constant/PreferenceKeys.kt
+++ app/src/main/java/com/dhp/musicplayer/constant/PreferenceKeys.kt
@@ -5,3 +5,4 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 
 val PersistentQueueDataKey = stringPreferencesKey("persistentQueueData")
 val RepeatModeKey = intPreferencesKey("repeatMode")
+val PlaylistViewTypeKey = stringPreferencesKey("playlistViewType")
