817ed07781c27a473cb5bdc16a818b72bd2875ee
dhp <june041996@gmail.com>
2024-05-31 16:33:06 +0700

    Add model get music api

---
+++ app/src/main/java/com/dhp/musicplayer/innertube/model/Continuation.kt
@@ -0,0 +1,17 @@
+package com.dhp.musicplayer.innertube.model
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonNames
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializable
+data class Continuation(
+    @JsonNames("nextContinuationData", "nextRadioContinuationData")
+    val nextContinuationData: Data?
+) {
+    @Serializable
+    data class Data(
+        val continuation: String?
+    )
+}
