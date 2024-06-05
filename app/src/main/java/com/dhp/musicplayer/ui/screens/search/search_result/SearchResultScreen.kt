13ef4afb3aa9d0d8580c8390c9aff9d14b374970
dhp <june041996@gmail.com>
2024-06-05 16:50:41 +0700

    Add a menu option and a long click listener for song search results

--- app/src/main/java/com/dhp/musicplayer/ui/screens/search/search_result/SearchResultScreen.kt
+++ app/src/main/java/com/dhp/musicplayer/ui/screens/search/search_result/SearchResultScreen.kt
@@ -1,8 +1,10 @@
 package com.dhp.musicplayer.ui.screens.search.search_result
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
@@ -11,6 +13,8 @@ import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -25,12 +29,16 @@ import com.dhp.musicplayer.R
 import com.dhp.musicplayer.constant.Dimensions
 import com.dhp.musicplayer.constant.ResultNavigationKey
 import com.dhp.musicplayer.extensions.asMediaItem
+import com.dhp.musicplayer.extensions.toSong
 import com.dhp.musicplayer.innertube.Innertube
 import com.dhp.musicplayer.innertube.InnertubeApiService
 import com.dhp.musicplayer.ui.AppState
+import com.dhp.musicplayer.ui.IconApp
 import com.dhp.musicplayer.ui.LocalPlayerConnection
 import com.dhp.musicplayer.ui.component.ChipsRow
 import com.dhp.musicplayer.ui.component.HandlePagingStates
+import com.dhp.musicplayer.ui.component.LocalMenuState
+import com.dhp.musicplayer.ui.component.MediaItemMenu
 import com.dhp.musicplayer.ui.items.SongItem
 import com.dhp.musicplayer.ui.screens.artist.navigation.navigateToArtistDetail
 import com.dhp.musicplayer.ui.screens.playlist.navigation.navigateToOnlinePlaylistDetail
@@ -40,7 +48,7 @@ import com.dhp.musicplayer.utils.getThumbnailInnertubeItem
 import com.dhp.musicplayer.utils.getTitleTextInnertubeItem
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalAnimationApi::class)
 @Composable
 fun SearchResultScreen(
     appState: AppState,
@@ -55,6 +63,7 @@ fun SearchResultScreen(
     val searchFilter by viewModel.searchFilter.collectAsState()
     val coroutineScope = rememberCoroutineScope()
     val musicApiService = InnertubeApiService.getInstance(LocalContext.current)
+    val menuState = LocalMenuState.current
 
     BackHandler {
         appState.navController.previousBackStackEntry?.savedStateHandle?.set(
@@ -114,20 +123,54 @@ fun SearchResultScreen(
                 val item = lazyPagingItems[index]
                 item?.let {
                     SongItem(
+                        id = item.key,
                         thumbnailUrl = getThumbnailInnertubeItem(item),
                         title = getTitleTextInnertubeItem(item),
-                        authors = getSubTitleTextInnertubeItem(item),
+                        subtitle = getSubTitleTextInnertubeItem(item),
                         duration = getSubTitleTextInnertubeItem(item),
                         isOffline = false,
                         bitmap = null,
                         thumbnailSizeDp = Dimensions.thumbnails.song,
                         trailingContent = {
+                            when (item) {
+                                is Innertube.SongItem -> {
+                                    Box {
+                                        IconButton(
+                                            onClick = {
+                                                menuState.show {
+                                                    MediaItemMenu(
+                                                        onDismiss = menuState::dismiss,
+                                                        mediaItem = item.asMediaItem
+                                                    )
+                                                }
+                                            }
+                                        ) {
+                                            Icon(
+                                                imageVector = IconApp.MoreVert,
+                                                contentDescription = null
+                                            )
+                                        }
+                                    }
+                                }
 
+                                else -> {}
+                            }
                         },
                         modifier = Modifier
                             .combinedClickable(
                                 onLongClick = {
+                                    when (item) {
+                                        is Innertube.SongItem -> {
+                                            menuState.show {
+                                                MediaItemMenu(
+                                                    onDismiss = menuState::dismiss,
+                                                    mediaItem = item.asMediaItem
+                                                )
+                                            }
+                                        }
 
+                                        else -> {}
+                                    }
                                 },
                                 onClick = {
                                     when (item) {
