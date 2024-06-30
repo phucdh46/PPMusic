3cd8e1ac1efef256afd976aeb86b19968ba4cdeb
dhp <june041996@gmail.com>
2024-06-30 11:30:02 +0700

    Improve logic for pagination in search results

--- feature/search/src/main/java/com/dhp/musicplayer/feature/search/search_result/SearchResultViewModel.kt
+++ feature/search/src/main/java/com/dhp/musicplayer/feature/search/search_result/SearchResultViewModel.kt
@@ -1,6 +1,7 @@
 package com.dhp.musicplayer.feature.search.search_result
 
 import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.snapshots.SnapshotStateMap
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -13,9 +14,12 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -30,31 +34,38 @@ class SearchResultViewModel @Inject constructor(
     val query: StateFlow<String?> = savedStateHandle.getStateFlow(SEARCH_RESULT_QUERY_ARG, null)
 
     private val filterMapSearchResult = mutableStateMapOf<String?, Flow<PagingData<Music>>?>()
+    private val _pagingData: MutableStateFlow<SnapshotStateMap<String?, Flow<PagingData<Music>>?>?> =
+        MutableStateFlow(null)
 
-    @OptIn(ExperimentalCoroutinesApi::class)
-    val pagingData = combine(query, searchFilter) { query, params ->
-        query to params
-    }.flatMapLatest { (query, params) ->
-        if (query != null) {
-            if (filterMapSearchResult[params] == null) {
-                filterMapSearchResult[params] =
-                    networkMusicRepository.getSearchResult(query, params, viewModelScope)
-            }
-            filterMapSearchResult[params] ?: flowOf(PagingData.empty())
-        } else {
-            flowOf(PagingData.empty())
-        }
+    val paramMapPagingData: StateFlow<SnapshotStateMap<String?, Flow<PagingData<Music>>?>?> =
+        _pagingData.asStateFlow()
+
+    init {
+        fetchPagingData()
     }
 
-    fun resetSearchFilter() {
-        filterMapSearchResult[searchFilter.value] = null
+    @OptIn(ExperimentalCoroutinesApi::class)
+    fun fetchPagingData() {
+        viewModelScope.launch {
+            combine(query, searchFilter) { query, params ->
+                query to params
+            }.flatMapLatest { (query, params) ->
+                if (query != null) {
+                    if (filterMapSearchResult[params] == null) {
+                        filterMapSearchResult[params] =
+                            networkMusicRepository.getSearchResult(query, params, viewModelScope)
+                    }
+                    filterMapSearchResult[params] ?: flowOf(null)
+                } else {
+                    flowOf(null)
+                }
+            }.collectLatest {
+                _pagingData.value = filterMapSearchResult
+            }
+        }
     }
 
     fun updateSearchFilter(params: String) {
         _searchFilter.value = params
-    }
-
-    fun onErrorInitPage() {
-        filterMapSearchResult[_searchFilter.value] = null
     }
 }
